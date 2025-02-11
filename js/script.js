ce6d2acdf40f73cd0b6061064903533326c7f6ba
GabrielRayssa <Gray180824@gmail.com>
2025-02-11 01:16:45 -0300

    botao de download e acesso senha melhorada responsividade

--- js/script.js
+++ js/script.js
@@ -1,51 +1,70 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     carregarFotos();
-    GerarImagem() 
+    GerarImagem()
+
+    const camposSenha = ["Senha1","Senha2","Senha3","Senha4"];
+    camposSenha.forEach(id =>{
+        const campo = document.getElementById(id);
+        campo.addEventListener("keydown", e => {
+            previousField(e.target, e);
+            campo.addEventListener("keyup", d => {
+                nextField(d.target, d);
+            });
+        });
+    });
 }, false);
-
-
-function Carregado(){
+function Carregado() {
     document.getElementById("Loader").style.display = 'none';
     document.getElementById("Senha").style.display = 'block';
     //para debug document.getElementById("Conteudo").style.display = 'block';
-
 }
-function nextField(current){
-
-    for (i = 0; i < current.form.elements.length; i++){
-        if (current.form.elements[i].tabIndex - current.tabIndex == 1){
-            current.form.elements[i].focus();
-            if (current.form.elements[i].type == "text"){
-                current.form.elements[i].select();
+function nextField(current, event) {
+    if (event.key != 'Backspace' && isFinite(event.key)) {
+        for (i = 0; i < current.form.elements.length; i++) {
+            if (current.form.elements[i].tabIndex - current.tabIndex == 1) {
+                current.form.elements[i].focus();
+                if (current.form.elements[i].type == "text") {
+                    current.form.elements[i].select();
+                }
             }
         }
     }
 }
-function Acessar(){
+function previousField(current, event) {
+    if (event.key == 'Backspace' && event.target.value=="") {
+        for (i = 0; i < current.form.elements.length; i++) {
+            if (current.form.elements[i].tabIndex - current.tabIndex == -1) {
+                current.form.elements[i].focus();
+                if (current.form.elements[i].type == "text") {
+                    current.form.elements[i].select();
+                }
+            }
+        }
+    }
+}
+function Acessar() {
     event.preventDefault()
     var numero1 = document.getElementById("Senha1").value;
     var numero2 = document.getElementById("Senha2").value;
     var numero3 = document.getElementById("Senha3").value;
     var numero4 = document.getElementById("Senha4").value;
-    if(numero1 == 1 && numero2 == 8 && numero3 == 0 && numero4 == 8)
-    {
+    if (numero1 == 1 && numero2 == 8 && numero3 == 0 && numero4 == 8) {
         document.getElementById("Loader").style.display = 'none';
         document.getElementById("Senha").style.display = 'none';
         document.getElementById("Conteudo").style.display = 'block';
     }
-    else
-    {
+    else {
         alert("Senha incorreta");
     }
 }
 
 
-window.onload = function(){
-    Carregado(); 
+window.onload = function () {
+    Carregado();
 }
 function contador() {
     var hoje = moment(new Date()); //"now"
-    var DataInicio = moment(new Date(2024, 7, 18));
+    var DataInicio = moment(new Date(2024, 7, 19));
 
     var duration = moment.duration(hoje.diff(DataInicio))
     if (duration.years() != 0) {
@@ -106,14 +125,14 @@ function scrollCarouselfotos(direction) {
         }%)`;
 }
 
-function carregarFotos(){
+function carregarFotos() {
     const imageContainer = document.getElementById('carouselfotos');
     let quantFotos = 17;
     for (let i = 0; i < quantFotos; i++) {
         const img = document.createElement('img'); // Create a new <img> element
-        img.src = 'fotos\\'+(i+1)+'.jpg'; // Set the source of the image
+        img.src = 'fotos\\' + (i + 1) + '.jpg'; // Set the source of the image
         img.alt = `Image ${i + 1}`; // Set an alt attribute for accessibility
-        img.className= 'fotos';
+        img.className = 'fotos';
         const div = document.createElement('div');
         div.className = 'relative w-full max-w-4xl mx-auto min-w-full';
         imageContainer.appendChild(div); // Append the <img> to the container
@@ -126,46 +145,45 @@ function GerarImagem() {
     var DataInicio = moment(new Date(2024, 7, 18));
     var duration = moment.duration(hoje.diff(DataInicio))
     var canvas = document.getElementById("Compartilhar");
-    if (canvas.getContext) 
-    {
-    var ctx = canvas.getContext("2d");
+    if (canvas.getContext) {
+        var ctx = canvas.getContext("2d");
 
-    // const image = document.getElementById("scream");
-    // image.addEventListener("load", (e) => {
-    //   ctx.drawImage(image, 10, 10);
-    // });
-    const gradient = ctx.createLinearGradient(0, 100, 200, 300);
-    gradient.addColorStop(0, '#991d99');
-    gradient.addColorStop(1, '#4b0a4b');
-    ctx.fillStyle = gradient;
-    ctx.fillRect(0, 0, 400, 400);
+        // const image = document.getElementById("scream");
+        // image.addEventListener("load", (e) => {
+        //   ctx.drawImage(image, 10, 10);
+        // });
+        const gradient = ctx.createLinearGradient(0, 100, 200, 300);
+        gradient.addColorStop(0, '#991d99');
+        gradient.addColorStop(1, '#4b0a4b');
+        ctx.fillStyle = gradient;
+        ctx.fillRect(0, 0, 400, 400);
 
-    ctx.fillStyle = 'black';
-    ctx.font = "bold 48px serif";
-    ctx.fillText("Rayssa e Gabriel", 25, 90);
+        ctx.fillStyle = 'black';
+        ctx.font = "bold 48px serif";
+        ctx.fillText("Rayssa e Gabriel", 25, 90);
 
-    ctx.fillStyle = 'white';
-    ctx.font = "bold 48px serif";
-    ctx.fillText("Rayssa e Gabriel", 22, 93);
-    
-    ctx.fillStyle = 'white';
-    ctx.font = "20px serif";
-    ctx.fillText(`juntos a:`, 160, 125);
+        ctx.fillStyle = 'white';
+        ctx.font = "bold 48px serif";
+        ctx.fillText("Rayssa e Gabriel", 22, 93);
 
-    ctx.fillStyle = 'white';
-    ctx.font = "30px serif";
-    ctx.fillText(`${duration.months()} meses`, 150, 160);
-    ctx.fillText(`${duration.days()} dias `, 152, 200);
-    ctx.fillText(`${duration.hours()} horas `, 150, 240);
-    ctx.fillText(`${duration.minutes()} minutos `, 130, 280);
-    
-    ctx.fillStyle = 'white';
-    ctx.font = "20px serif";
-    ctx.fillText(`e contando...`, 150, 310);
+        ctx.fillStyle = 'white';
+        ctx.font = "20px serif";
+        ctx.fillText(`juntos a:`, 160, 125);
+
+        ctx.fillStyle = 'white';
+        ctx.font = "30px serif";
+        ctx.fillText(`${duration.months()} meses`, 150, 160);
+        ctx.fillText(`${duration.days()} dias `, 152, 200);
+        ctx.fillText(`${duration.hours()} horas `, 150, 240);
+        ctx.fillText(`${duration.minutes()} minutos `, 130, 280);
+
+        ctx.fillStyle = 'white';
+        ctx.font = "20px serif";
+        ctx.fillText(`e contando...`, 150, 310);
+    }
 }
-}
 
-function BaixarImagem(){
+function BaixarImagem() {
     let canvasImage = document.getElementById('Compartilhar').toDataURL('image/png');
     let xhr = new XMLHttpRequest();
     xhr.responseType = 'blob';
@@ -177,7 +195,7 @@ function BaixarImagem(){
         document.body.appendChild(a);
         a.click();
         a.remove();
-      };
-      xhr.open('GET', canvasImage); // This is to download the canvas Image
-      xhr.send();
+    };
+    xhr.open('GET', canvasImage); // This is to download the canvas Image
+    xhr.send();
 }
